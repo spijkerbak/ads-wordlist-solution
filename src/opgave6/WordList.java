3226fddf3e6b05edd95578886633b2e579b9a1c7
Frans Spijkerman <fj.spijkerman@avans.nl>
2018-12-21 10:12:56 +0100

    initial

---
+++ src/opgave6/WordList.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opgave6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class WordList {
+    // interne list: 
+    // - moet gesorteerd blijven na elke operatie
+    // - mag geen dubbele woorden bevatten
+
+    private List<String> list;
+
+    String get(int i) {
+        return list.get(i);
+    }
+
+    int size() {
+        return list.size();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Run the tests, please!");
+    }
+
+    WordList(String[] input) {
+        // bubble-sort the input array
+        for (int i = 0; i < input.length - 1; i++) {
+            for (int j = 0; j < input.length - i - 1; j++) {
+                if (input[j + 1].compareTo(input[j]) < 0) {
+                    String h = input[j];
+                    input[j] = input[j + 1];
+                    input[j + 1] = h;
+                }
+            }
+        }
+        list = Arrays.asList(input);
+    }
+
+    WordList(WordList a, WordList b) {
+        // merge-sort:
+        list = new ArrayList<>();
+        int aLen = a.list.size();
+        int bLen = b.list.size();
+        int i = 0, j = 0;
+        while (i < aLen && j < bLen) {
+            int d = a.list.get(i).compareTo(b.list.get(j));
+            if (d == 0) {
+                list.add(a.list.get(i++));
+                j++;
+            } else if (d < 0) {
+                list.add(a.list.get(i++));
+            } else if (d > 0) {
+                list.add(b.list.get(j++));
+            }
+        }
+        while (i < aLen) {
+            list.add(a.list.get(i++));
+        }
+        while (j < bLen) {
+            list.add(b.list.get(j++));
+        }
+    }
+
+    boolean hasWord(String word) {
+        // binary search
+        int first = 0;
+        int last = list.size() - 1;
+        while (first <= last) {
+            int mid = (first + last) / 2;
+            int dif = list.get(mid).compareTo(word);
+            if (dif < 0) {
+                first = mid + 1;
+            } else if (dif > 0) {
+                last = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        // hash per element - exactly what we need
+        return this.list.hashCode();
+
+        // return 1; // would work, but makes hashMap one long list
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        final WordList that = (WordList) obj;
+        // equals per element - exactly what we need
+        return this.list.equals(that.list);
+    }
+
+    private boolean equalsAlternative(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final WordList that = (WordList) obj;
+
+        if (this.list.size() != that.list.size()) {
+            return false;
+        }
+        for (int i = 0; i < this.list.size(); i++) {
+            if (!this.list.get(i).equals(that.list.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ test/opgave6/WordListTest.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opgave6;
+
+import java.util.HashSet;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Frans
+ */
+public class WordListTest {
+
+    public WordListTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testContructor1() {
+        System.out.println("test 1: first constructor");
+
+        WordList planetsA = new WordList(new String[]{"Mars", "Aarde", "Venus"});
+        assertEquals(planetsA.size(), 3);
+        assertEquals(planetsA.get(0), "Aarde");
+        assertEquals(planetsA.get(1), "Mars");
+        assertEquals(planetsA.get(2), "Venus");
+
+        WordList planetsB = new WordList(new String[]{"Venus", "Aarde", "Jupiter", "Mars"});
+        assertEquals(planetsB.size(), 4);
+        assertEquals(planetsB.get(0), "Aarde");
+        assertEquals(planetsB.get(1), "Jupiter");
+        assertEquals(planetsB.get(2), "Mars");
+        assertEquals(planetsB.get(3), "Venus");
+    }
+
+    @Test
+    public void testHasWord() {
+        System.out.println("test 2: hasWord");
+
+        WordList planets = new WordList(new String[]{"Mars", "Aarde", "Venus"});
+        assertTrue(planets.hasWord("Mars"));
+        assertTrue(planets.hasWord("Aarde"));
+        assertTrue(planets.hasWord("Venus"));
+        assertFalse(planets.hasWord("Jupiter"));
+        assertFalse(planets.hasWord("Pluto"));
+        assertFalse(planets.hasWord("Zeist"));
+
+    }
+
+    @Test
+    public void testHashSet() {
+        System.out.println("test 3: hashSet");
+        WordList planetsA = new WordList(new String[]{"Mars", "Aarde", "Venus"});
+        WordList planetsB = new WordList(new String[]{"Aarde", "Mars", "Venus"});
+        WordList planetsC = new WordList(new String[]{"Aarde", "Mars", "Jupiter"});
+
+        HashSet<WordList> listSet = new HashSet<>();
+
+        listSet.add(planetsA);
+        assertEquals(listSet.size(), 1);
+
+        listSet.add(planetsB); // zou geen effect moeten hebben
+        assertEquals(listSet.size(), 1);
+
+        listSet.add(planetsC); // zou wel effect moeten hebben
+        assertEquals(listSet.size(), 2);
+
+        listSet.add(planetsC); // zou geen effect moeten hebben
+        assertEquals(listSet.size(), 2);
+    }
+    
+    @Test
+    public void testContructor2() {
+        System.out.println("test 4: extra constructor");
+        WordList planetsA = new WordList(new String[]{"Mars", "Aarde", "Saturnus", "Pluto", "Venus"});
+        WordList planetsB = new WordList(new String[]{"Aarde", "Mars", "Venus", "Jupiter"});
+        WordList planetsC = new WordList(planetsA, planetsB);
+        assertEquals(planetsC.size(), 6);
+        assertEquals(planetsC.get(0), "Aarde");
+        assertEquals(planetsC.get(1), "Jupiter");
+        assertEquals(planetsC.get(2), "Mars");
+        assertEquals(planetsC.get(3), "Pluto");
+        assertEquals(planetsC.get(4), "Saturnus");
+        assertEquals(planetsC.get(5), "Venus");
+    }
+
+}
